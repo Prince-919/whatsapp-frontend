0bba4c54bf91af1380ee399e76ebf59e2ebae1f3
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2023-11-03 01:45:10 +0530

    get conversations

--- src/features/chatSlice.js
+++ src/features/chatSlice.js
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const CONVERSATION_ENDPOINT = `${process.env.REACT_APP_API_ENDPOINT}/conversation`;
 
 const initialState = {
   status: "",
@@ -8,6 +11,23 @@ const initialState = {
   notifications: [],
 };
 
+// Get Conversation
+export const getConversations = createAsyncThunk(
+  "conversation/all",
+  async (token, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(CONVERSATION_ENDPOINT, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data.error.message);
+    }
+  }
+);
+
 const chatSlice = createSlice({
   name: "chat",
   initialState,
@@ -15,6 +35,20 @@ const chatSlice = createSlice({
     setActiveConversation: (state, action) => {
       state.activeConversation = action.payload;
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getConversations.pending, (state, action) => {
+        state.status = "pending";
+      })
+      .addCase(getConversations.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.conversations = action.payload;
+      })
+      .addCase(getConversations.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
   },
 });
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Sidebar } from "../components/sidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { getConversations } from "../features/chatSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (user?.token) {
+      dispatch(getConversations(user.token));
+    }
+  }, [user]);
   return (
     <div className="min-h-screen dark:bg-dark_bg_1 flex justify-center items-center py-[19px] overflow-hidden">
       <div className="container min-h-screen flex">
