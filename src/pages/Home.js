3ed7232b982b19673373c2316b0f929ffc48fc35
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2023-11-03 00:58:53 +0530

    build sidebar & header & notifications & search bar UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Sidebar } from "../components/sidebar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen dark:bg-dark_bg_1 flex justify-center items-center py-[19px] overflow-hidden">
+      <div className="container min-h-screen flex">
+        <Sidebar />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
